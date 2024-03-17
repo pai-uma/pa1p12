37c74aa4cf01816a821c2f41d7a8440abc2cbfda
Ernesto Pimentel <pi@ernestopimentel.es>
2024-03-17 19:34:26 +0100

    Incluyendo los libros a añadir en PruebaLibreria.java

--- src/main/java/PruebaLibreria.java
+++ src/main/java/PruebaLibreria.java
@@ -1,6 +1,19 @@
 
 public class PruebaLibreria1 {
-
+	/* Libros a añadir
+	("george orwell", "1984", 8.20)
+	("Philip K. Dick", "?Suenan los androides con ovejas electricas?", 3.50)
+	("Isaac Asimov", "Fundacion e Imperio", 9.40)
+	("Ray Bradbury", "Fahrenheit 451", 7.40)
+	("Aldous Huxley", "Un Mundo Feliz", 6.50)
+	("Isaac Asimov", "La Fundacion", 7.30)
+	("William Gibson", "Neuromante", 8.30)
+	("Isaac Asimov", "Segunda Fundacion", 8.10)
+	("Isaac Newton", "arithmetica universalis", 7.50)
+	("George Orwell", "1984", 6.20)
+	("Isaac Newton", "Arithmetica Universalis", 10.50)
+	*/
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
