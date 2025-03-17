d257501607d572b7ac3578011ce807d93d886f39
Ernesto Pimentel <pi@ernestopimentel.es>
2025-03-17 14:29:42 +0100

    Update PruebaLibreria.java

--- src/main/java/PruebaLibreria.java
+++ src/main/java/PruebaLibreria.java
@@ -2,13 +2,13 @@
 public class PruebaLibreria {
 	/* Libros a añadir
 	("george orwell", "1984", 8.20)
-	("Philip K. Dick", "?Suenan los androides con ovejas electricas?", 3.50)
-	("Isaac Asimov", "Fundacion e Imperio", 9.40)
+	("Philip K. Dick", "¿Sueñan los androides con ovejas eléctricas?", 3.50)
+	("Isaac Asimov", "Fundación e Imperio", 9.40)
 	("Ray Bradbury", "Fahrenheit 451", 7.40)
 	("Aldous Huxley", "Un Mundo Feliz", 6.50)
-	("Isaac Asimov", "La Fundacion", 7.30)
+	("Isaac Asimov", "La Fundación", 7.30)
 	("William Gibson", "Neuromante", 8.30)
-	("Isaac Asimov", "Segunda Fundacion", 8.10)
+	("Isaac Asimov", "Segunda Fundación", 8.10)
 	("Isaac Newton", "arithmetica universalis", 7.50)
 	("George Orwell", "1984", 6.20)
 	("Isaac Newton", "Arithmetica Universalis", 10.50)
